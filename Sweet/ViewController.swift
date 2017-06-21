7ad0feac21997ae740db3f806bca2ee8df191556
miho n <miho@nishiharamihonoMacBook.local>
2017-06-21 14:15:52 +0900

    Initial Commit

---
+++ Sweet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sweet
+//
+//  Created by miho on 2017/06/21.
+//  Copyright © 2017年 miho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
